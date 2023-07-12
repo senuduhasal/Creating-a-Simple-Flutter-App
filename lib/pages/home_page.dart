89000a0eb6d046bdc7785c9e2232df81449f6828
senuduhasal <senuduhasal@gmail.com>
2023-07-12 20:35:04 +0530

    First Flutter App

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("This is Title bar")
+      ),
+      body: const Center(
+        child: Text("This is Center"),
+      ),
+    );
+  }
+}
